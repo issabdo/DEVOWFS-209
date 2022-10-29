8ae3275c8abb7159c85ceaf27759605745231da1
issabdo <issabdo0011@gmail.com>
2022-10-29 12:29:34 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>DEV OW FS 208</title>
+</head>
+<body>
+<h1>DEV OW FS 208</h1>
+
+</body>
+</html>
