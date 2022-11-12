9c77f52083948d812eabea14f92bab3ef23f1f8b
Adam-Hajji <adamhajji56@gmail.com>
2022-11-12 13:54:19 +0100

    first commit

---
+++ adam.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>DEV OW FS 209</title>
+</head>
+<body>
+    <h1>Adam Hajji</h1>
+    <h2>DEV OW FS 209</h2>
+
+</body>
+</html>
